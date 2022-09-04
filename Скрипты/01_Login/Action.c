c0412794e1d68d88d8131acb65861e2eeba31117
Vasiliy Zhuravlev <111848086+Zhuraman@users.noreply.github.com>
2022-09-04 09:41:13 +0300

    Скрипты

---
+++ Скрипты/01_Login/Action.c
@@ -0,0 +1,147 @@
+Action()
+{
+
+	/*Possible OAUTH authorization was detected. It is recommended to correlate the authorization parameters.*/
+
+	lr_start_transaction("01_login");
+	
+	lr_start_transaction("open_home_page");
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_header("sec-ch-ua", 
+		"\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"");
+
+	web_add_header("sec-ch-ua-mobile", 
+		"?0");
+
+	web_add_header("sec-ch-ua-platform", 
+		"\"Windows\"");
+	
+	web_reg_find("Fail=NotFound",
+		"Text/IC=A Session ID has been created and loaded into a cookie called MSO",
+		LAST);
+	
+	
+/*Correlation comment - Do not change!  Original value='134598.866990684zQcDtcQpccAiDDDDtVftApfHfDcf' Name ='userSession' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+
+	web_url("welcome.pl", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/WebTours/", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+	lr_end_transaction("open_home_page", LR_AUTO);
+
+	/* LOGIN */
+
+	lr_start_transaction("login");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_auto_header("sec-ch-ua", 
+		"\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"");
+
+	web_add_auto_header("sec-ch-ua-mobile", 
+		"?0");
+
+	web_add_auto_header("sec-ch-ua-platform", 
+		"\"Windows\"");
+
+	lr_think_time(5);
+	
+
+	web_reg_find("Fail=NotFound",
+		"Text/IC=User password was correct",
+		LAST);
+	
+	web_reg_find("Fail=NotFound",
+		"Text=Welcome, <b>{Login}</b>",
+		LAST);
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={Login}", ENDITEM,
+		"Name=password", "Value={Password}", ENDITEM,
+		"Name=login.x", "Value=47", ENDITEM,
+		"Name=login.y", "Value=7", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+	lr_end_transaction("login",LR_AUTO);
+
+	lr_start_transaction("logout");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	lr_think_time(5);
+	
+	web_reg_find("Fail=NotFound",
+		"Text/IC=A Session ID has been created and loaded into a cookie called MSO",
+		LAST);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("logout",LR_AUTO);
+	
+	lr_end_transaction("01_login",LR_AUTO);
+
+	return 0;
+}
